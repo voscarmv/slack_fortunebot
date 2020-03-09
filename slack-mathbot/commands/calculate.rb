b8ce6f9d7d5dc2c6add73112705c06034772cf8d
Oscar Mier <voscarmv@gmail.com>
2020-03-09 15:47:16 -0600

    Basic Input/Output achieved.

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,7 +2,9 @@ module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+        result = _match[:expression]
+        #client.say(channel: data.channel, text: "#{result} !!!")
+        client.say(channel: data.channel, text: "#{_match[:command]}: #{_match[:expression]}")
       end
     end
   end
