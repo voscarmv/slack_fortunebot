a935ed9c6b7ce5b84a87cf1859cefb5ec7584911
Oscar Mier <voscarmv@gmail.com>
2020-03-10 11:45:18 -0600

    First successful stackoverflow query with bot.

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -1,10 +1,12 @@
+require_relative '../../stack-overflow-command-line/lib/stackoverflow.rb'
+
 module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
         result = _match[:expression]
         #client.say(channel: data.channel, text: "#{result} !!!")
-        client.say(channel: data.channel, text: "#{_match[:command]}: #{_match[:expression]}")
+        client.say(channel: data.channel, text: StackOverflowMod::Command.new.run("html css"))
       end
     end
   end
